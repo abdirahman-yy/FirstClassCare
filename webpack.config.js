5d18a4c183eaedf391e49efef2f9082287878b9c
Abdirahman Mohamed <abdirahman@Abdirahmans-MacBook-Pro.local>
2025-05-13 23:52:36 -0500

    fix(build): update webpack config for proper asset handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.[contenthash].js',
-    publicPath: process.env.NODE_ENV === 'production' 
-      ? './' 
-      : '/'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@styles': path.resolve(__dirname, 'src/styles')
-    }
+    publicPath: '/',
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]'
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
         use: 'ts-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/assets/images/logo.png'
+      template: 'public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: 'public',
+          to: '',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
     historyApiFallback: true,
-    port: 3004,
-    open: true,
-    hot: true,
+    compress: true,
+    port: 3000,
   },
 }; 
